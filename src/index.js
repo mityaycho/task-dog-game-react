48fd2099364b715488b3cb14be7c82f97c9682e8
mityaycho <mityaycho@gmail.com>
2019-12-30 23:21:55 +0300

    add some changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/store';
-import {Provider} from "react-redux";
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
   <Provider store={store}>
