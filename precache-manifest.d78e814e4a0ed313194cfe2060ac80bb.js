5bbebc7d0bacab55c2fadfb30fb107ef987f735e
mityaycho <mityaycho@gmail.com>
2020-08-14 14:43:27 +0300

    Updates

---
+++ precache-manifest.d78e814e4a0ed313194cfe2060ac80bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edab08108e64cebce7f36a94d1ff5be6",
+    "url": "/task-dog-game-react/index.html"
+  },
+  {
+    "revision": "0a8c6f7a9f7a517ac3f6",
+    "url": "/task-dog-game-react/static/css/main.c6668c33.chunk.css"
+  },
+  {
+    "revision": "f38338a47283b795a0ec",
+    "url": "/task-dog-game-react/static/js/2.caef6169.chunk.js"
+  },
+  {
+    "revision": "0a8c6f7a9f7a517ac3f6",
+    "url": "/task-dog-game-react/static/js/main.8dec57db.chunk.js"
+  },
+  {
+    "revision": "e1fa02d38cced82e167b",
+    "url": "/task-dog-game-react/static/js/runtime-main.d26e22e4.js"
+  },
+  {
+    "revision": "75fa68d4d95a95385dabee5a76f50797",
+    "url": "/task-dog-game-react/static/media/dog.75fa68d4.mp3"
+  },
+  {
+    "revision": "0092a09f635306305d5fb4410f1d09f0",
+    "url": "/task-dog-game-react/static/media/sad-dog.0092a09f.mp3"
+  }
+]);
